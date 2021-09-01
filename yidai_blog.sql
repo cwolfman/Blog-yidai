c6cd7683126ac531ebf196b262413d4121cbea5b
一呆 <2376667448@qq.com>
2021-09-02 00:39:43 +0800

    前端页面优化

--- yidai_blog.sql
+++ yidai_blog.sql
@@ -97,6 +97,21 @@ CREATE TABLE `t_type`
   COLLATE = utf8_general_ci
   ROW_FORMAT = COMPACT;
 
+
+-- -----------------------------------------
+DROP TABLE IF EXISTS `t_wallpaper`;
+
+CREATE TABLE `t_wallpaper`
+(
+    `id` BIGINT(200) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+    PRIMARY KEY (`id`) USING BTREE
+) ENGINE = INNODB
+  AUTO_INCREMENT = 9
+  CHARACTER SET = utf8
+  COLLATE = utf8_general_ci
+  ROW_FORMAT = COMPACT;
+
 -- -----------------------------------------
 DROP TABLE IF EXISTS `t_user`;
 
